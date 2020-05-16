f71cfaa3230e306a887c24b339cc8ba38aa0b6bb
nicolaslvy <nicolasmortizg@gmail.com>
2020-05-16 18:28:22 -0500

    first commit

---
+++ precache-manifest.e2a3859f2717bcaefe81bb89783c7275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5335a1a34264f87a0027426311eee6a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c85d0ef8093f45b1bc6a",
+    "url": "/static/css/main.7003bcf3.chunk.css"
+  },
+  {
+    "revision": "ddb4dcfd9564029f7710",
+    "url": "/static/js/2.088b57a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.088b57a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85d0ef8093f45b1bc6a",
+    "url": "/static/js/main.5a09eb54.chunk.js"
+  },
+  {
+    "revision": "dac00261a85842d45d01",
+    "url": "/static/js/runtime-main.8b421a76.js"
+  }
+]);
