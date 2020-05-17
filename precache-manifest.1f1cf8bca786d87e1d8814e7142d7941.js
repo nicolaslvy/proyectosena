8e229e6470e27b5693b6fc1ef9e46e02f58ca517
nicolaslvy <nicolasmortizg@gmail.com>
2020-05-17 13:35:30 -0500

    Updates

---
+++ precache-manifest.1f1cf8bca786d87e1d8814e7142d7941.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ad4d8f99f1f853ed7916533f1d4fc0",
+    "url": "/proyectosena/index.html"
+  },
+  {
+    "revision": "ce0512f2d719c9a0e989",
+    "url": "/proyectosena/static/css/main.7003bcf3.chunk.css"
+  },
+  {
+    "revision": "ddb4dcfd9564029f7710",
+    "url": "/proyectosena/static/js/2.088b57a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/proyectosena/static/js/2.088b57a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0512f2d719c9a0e989",
+    "url": "/proyectosena/static/js/main.289014b7.chunk.js"
+  },
+  {
+    "revision": "f83c0c79ceb7109d22d5",
+    "url": "/proyectosena/static/js/runtime-main.c462d256.js"
+  }
+]);
